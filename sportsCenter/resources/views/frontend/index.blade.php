6afe7cd6827c06bf6e5afb6e3ebe564ed20e10bf
DarijoTepic <darijotepic@hotmail.com>
2020-11-04 12:59:12 +0100

    Dodani svi fajlovi

---
+++ sportsCenter/resources/views/frontend/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.frontend')
+
+@section('title')
+    Home
+@endsection
+
+@section('content')
+
+    @include('frontend.slider')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1>Dobrodosli u Sportski centar Spartak !</h1>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
+
+@section('scripts')
+
+@endsection
