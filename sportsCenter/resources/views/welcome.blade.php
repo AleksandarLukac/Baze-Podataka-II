7862dc12fef0e0281c522f1ce47c7fa50bc75f81
DarijoTepic <darijotepic@hotmail.com>
2020-11-04 12:53:47 +0100

    Prvi komit moje grane!

--- sportsCenter/resources/views/welcome.blade.php
+++ sportsCenter/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel moj
                 </div>
 
                 <div class="links">
